9531425de6363361c31f8fef4ce0895847d96415
lixiang12138 <zhao20130673@163.com>
2019-06-18 10:48:28 +0800

    基岩 alpha
    基岩

---
+++ go.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+cd touhoufront
+npm run build
+cd ..
---
+++ src/main/java/com/ecust/touhouairline/TouhouairlineApplication.java
@@ -0,0 +1,13 @@
+package com.ecust.touhouairline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouhouairlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TouhouairlineApplication.class, args);
+    }
+
+}
