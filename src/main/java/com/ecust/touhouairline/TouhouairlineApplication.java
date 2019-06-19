798d80158d9eaf3ef68c7ff4a3147678f4c2e03a
lixiang12138 <zhao20130673@163.com>
2019-06-19 17:36:37 +0800

    For lixiao's controller

--- src/main/java/com/ecust/touhouairline/TouhouairlineApplication.java
+++ src/main/java/com/ecust/touhouairline/TouhouairlineApplication.java
@@ -2,13 +2,14 @@ package com.ecust.touhouairline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class TouhouairlineApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TouhouairlineApplication.class, args);
     }
-    //test
-
 }
