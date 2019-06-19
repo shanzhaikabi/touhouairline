a96e9fc7c0d6f4f8d4a624d2f034a9343ca45e55
shanzhaikabi <shanzhaikabi@users.noreply.github.com>
2019-06-19 17:44:10 +0800

    更改了repository

--- src/main/java/com/ecust/touhouairline/TouhouairlineApplication.java
+++ src/main/java/com/ecust/touhouairline/TouhouairlineApplication.java
@@ -1,14 +1,21 @@
 package com.ecust.touhouairline;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.jpa.HibernateEntityManagerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+import javax.persistence.EntityManagerFactory;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableJpaRepositories
 public class TouhouairlineApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TouhouairlineApplication.class, args);
     }
-    //test1
-
 }
--- src/main/java/com/ecust/touhouairline/repository/DemoRepository.java
+++ src/main/java/com/ecust/touhouairline/repository/DemoRepository.java
@@ -1,11 +1,12 @@
 package com.ecust.touhouairline.repository;
 
 import com.ecust.touhouairline.entity.DemoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DemoRepository extends BaseRepository<DemoEntity,Integer> {
-    /**
-     * 示例函数，什么都不干（
-     * @param demo
-     */
-    void demo(DemoEntity demo);
+import java.util.Optional;
+
+public interface DemoRepository extends JpaRepository<DemoEntity,Integer> {
+
+    Optional<DemoEntity> findById(Integer id);
+    <S extends DemoEntity> S save(S s);
 }
