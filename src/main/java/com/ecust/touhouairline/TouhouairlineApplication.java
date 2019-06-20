3e66550ee1f200f86f22b151b607313e7f3ef21e
lixiang12138 <zhao20130673@163.com>
2019-06-20 09:34:47 +0800

    REVERT

--- src/main/java/com/ecust/touhouairline/TouhouairlineApplication.java
+++ src/main/java/com/ecust/touhouairline/TouhouairlineApplication.java
@@ -3,15 +3,13 @@ package com.ecust.touhouairline;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-@EnableJpaRepositories
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class TouhouairlineApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TouhouairlineApplication.class, args);
     }
-    //test
-
 }
--- src/main/java/com/ecust/touhouairline/controllers/TestLogin.java
+++ src/main/java/com/ecust/touhouairline/controllers/TestLogin.java
@@ -1,7 +1,5 @@
-package com.ecust.touhouairline.controller;
+package com.ecust.touhouairline.controllers;
 
-import com.ecust.touhouairline.service.DemoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,23 +13,13 @@ import java.util.List;
 
 @RestController
 public class TestLogin {
-
-    @Autowired
-    DemoService demoService;
-
     @RequestMapping(value="test",method = RequestMethod.GET)
     public ModelMap showCharacter(String id){
+        System.out.println("114514");
         ModelMap map = new ModelMap();
-<<<<<<< HEAD:src/main/java/com/ecust/touhouairline/controllers/TestLogin.java
         if(id!=null && id.equals("12345")) {
             map.put("result", "success");
             System.out.println("24岁");
-=======
-        System.out.println("114514");
-        if(id.equals("12345")){
-            map.put("result",demoService.getDemoById(Integer.valueOf(id)));
-            System.out.println(demoService.getDemoById(Integer.valueOf(id)).getName());
->>>>>>> e29f349b0f4a3cecf79b89e3f0bc8e42c87998f9:src/main/java/com/ecust/touhouairline/controller/TestLogin.java
         }
         return map;
     }
