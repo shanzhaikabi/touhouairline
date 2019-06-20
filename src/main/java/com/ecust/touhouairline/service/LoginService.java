0488da1bbe17ba10ec7256bc21a15dfb296dbff8
Desicool <609936947@qq.com>
2019-06-20 13:36:02 +0800

    添加了service类的Javadoc注释 添加了一个TODO

--- src/main/java/com/ecust/touhouairline/service/LoginService.java
+++ src/main/java/com/ecust/touhouairline/service/LoginService.java
@@ -8,10 +8,21 @@ import org.springframework.stereotype.Service;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * @author 姚迟亮
+ * 创建日期：2019-6-19
+ **/
 @Service
 public class LoginService {
+    //TODO：把TmpEntity和TmpRepository改为实际使用的Entity和Repository类
     @Autowired
     private TmpRepository tmpRepository;
+
+    /**
+     * @param AccountName 账户名
+     * @param Password 密码
+     * @return 成功返回Success，失败返回"用户名或密码错误"
+     */
     String Login(String AccountName,String Password){
         Map<String,String> Ret = new HashMap<>();
         TmpEntity User = tmpRepository.findByAccountNameAndPassword(AccountName,Password);
