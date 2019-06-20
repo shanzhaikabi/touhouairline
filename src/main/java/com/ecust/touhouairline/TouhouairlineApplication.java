2d0d70847e322f8c0a8c371fb912fb6572358689
lixiang12138 <zhao20130673@163.com>
2019-06-20 09:34:47 +0800

    REVERT

--- src/main/java/com/ecust/touhouairline/TouhouairlineApplication.java
+++ src/main/java/com/ecust/touhouairline/TouhouairlineApplication.java
@@ -1,18 +1,12 @@
 package com.ecust.touhouairline;
 
-import org.hibernate.SessionFactory;
-import org.hibernate.jpa.HibernateEntityManagerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import javax.persistence.EntityManagerFactory;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-@EnableJpaRepositories
 public class TouhouairlineApplication {
 
     public static void main(String[] args) {
