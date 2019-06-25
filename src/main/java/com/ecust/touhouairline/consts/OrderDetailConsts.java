728c9a1bad706eff7df1e7ee83fee561927e752c
Desicool <609936947@qq.com>
2019-06-25 17:26:59 +0800

    为了防止删除orderdetail中的orderNo出现问题的commit

---
+++ src/main/java/com/ecust/touhouairline/consts/OrderDetailConsts.java
@@ -0,0 +1,9 @@
+package com.ecust.touhouairline.consts;
+
+public final class OrderDetailConsts {
+    public static final String ADULT = "成人";
+    public static final String CHILD = "儿童";
+
+    public static final String BEFORE_CHECK_IN = "未取票";
+    public static final String AFTER_CHECK_IN = "已取票";
+}
