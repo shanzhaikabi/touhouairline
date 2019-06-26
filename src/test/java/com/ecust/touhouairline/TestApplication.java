c462661d2a40818008c0203e5f679acc1d727c08
shanzhaikabi <shanzhaikabi@users.noreply.github.com>
2019-06-26 10:00:35 +0800

    more controllers

--- src/test/java/com/ecust/touhouairline/TestApplication.java
+++ src/test/java/com/ecust/touhouairline/TestApplication.java
@@ -2,14 +2,22 @@ package com.ecust.touhouairline;
 
 import com.alibaba.fastjson.JSON;
 import com.ecust.touhouairline.entity.UserEntity;
+import com.ecust.touhouairline.repository.UserRepository;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class TestApplication {
     @Test
     public void test(){
+
         // 将Json字符串反序列化为Java对象
-        UserEntity user = JSON.parseObject("{username:\"test\",password:\"123456\"}", UserEntity.class);
-        System.out.println(user);
+
+        System.out.println();
     }
 
 }
--- src/test/java/com/ecust/touhouairline/controller/TestController.java
+++ src/test/java/com/ecust/touhouairline/controller/TestController.java
@@ -1,25 +1,29 @@
 package com.ecust.touhouairline.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.ecust.touhouairline.entity.FlightEntity;
+import com.ecust.touhouairline.entity.UserEntity;
 import com.ecust.touhouairline.repository.FlightRepository;
+import com.ecust.touhouairline.repository.UserRepository;
 import com.ecust.touhouairline.service.QueryFlightService;
+import org.apache.catalina.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Optional;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TestController{
     @Autowired
-    private QueryFlightService queryFlightService;
-    @Autowired
     private FlightRepository flightRepository;
     @Test
-    public void queryFlightTest(){
-        FlightEntity flightEntity = flightRepository.findAll().get(0);
-        queryFlightService.showDetailByFlight(flightEntity);
+    public void Test(){
+        FlightEntity test = flightRepository.findById("1").get();
+        System.out.println(JSON.toJSONString(test));
     }
 }
